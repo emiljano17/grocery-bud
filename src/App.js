6602383b94046ced4749acac86243a1019d26fc9
Emiljano <emiljanodemirxhiu15@gmail.com>
2022-03-24 23:03:13 +0100

    new branch

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ function App() {
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
         {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-        <h3>grocery bud</h3>
+        <h3>Grocery Bud</h3>
         <div className="form-control">
           <input
             type="text"
